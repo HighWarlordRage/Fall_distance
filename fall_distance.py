bc1bb2ab303ff2ee4b0ab5b91e6eb59ecb23338f
HighWarlordRage <hussaias@oregonstate.edu>
2021-07-10 00:55:10 +0500

    Initial commit

---
+++ fall_distance.py
@@ -0,0 +1,12 @@
+#Title: Project-4a
+
+#Defining a Function
+def fall_distance(t):
+    """"A function named fall_distance that takes the time in seconds as an argument"""
+    g = 9.8
+    d = (1/2)*g*t*t
+    #print(d)
+    return d
+#calling the function
+#fall_distance(3.2)
+
